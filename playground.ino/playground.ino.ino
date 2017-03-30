226f65e7e06cb766449df16f711c3031b421af5d
Seungjin Kim <skim@proove.com>
2017-03-29 23:56:56 -0700

    led_strip: strip out all code and leave the led strip code

--- playground.ino/playground.ino.ino
+++ playground.ino/playground.ino.ino
@@ -20,202 +20,59 @@
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 
-MDNSResponder mdns;
-ESP8266WebServer server(80);
+//MDNSResponder mdns;
+//ESP8266WebServer server(80);
 
-String webPage = "";
+//String webPage = "";
 
-int gpio0_pin = 0;
-int gpio2_pin = 2;
+//int gpio0_pin = 0;
+//int gpio2_pin = 2;
+int d1 = 5;
+int d2 = 4;
+int d3 = 0;
+int d4 = 2;
+int r_pin = d2;
+int g_pin = d1;
+int b_pin = d3;
+int w_pin = d4; 
 
-#define PIN 4
-const char* ssid     = "imo";
-const char* password = "dlahdlah";
-
-/**** NTP TIME SETTING LOGIC ***/
-WiFiUDP udp;
-unsigned int localPort = 2390;
-IPAddress timeServerIP;
-const char* ntpServerName = "time.nist.gov";
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-
-void settime(){
-  Serial.println("Starting UDP for NTP connection..");
-  udp.begin(localPort);
-  Serial.print("Local port: ");
-  Serial.println(udp.localPort());
-  // setup done.
-}
-
-void gettime(){
-    //get a random server from the pool
-  WiFi.hostByName(ntpServerName, timeServerIP); 
-
-  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
-  // wait to see if a reply is available
-  delay(1000);
-  
-  int cb = udp.parsePacket();
-  if (!cb) {
-    Serial.println("no packet yet");
-  }
-  else {
-    Serial.print("packet received, length=");
-    Serial.println(cb);
-    // We've received a packet, read the data from it
-    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-
-    //the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
-
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = " );
-    Serial.println(secsSince1900);
-
-    // now convert NTP time into everyday time:
-    Serial.print("Unix time = ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    // subtract seventy years:
-    unsigned long epoch = secsSince1900 - seventyYears;
-    // print Unix time:
-    Serial.println(epoch);
-
-
-    // print the hour, minute and second:
-    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
-    Serial.print(':');
-    if ( ((epoch % 3600) / 60) < 10 ) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-    Serial.print(':');
-    if ( (epoch % 60) < 10 ) {
-      // In the first 10 seconds of each minute, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.println(epoch % 60); // print the second
-  }
-  // wait ten seconds before asking for the time again
-  delay(10000);
-  
-}
-// send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(IPAddress& address)
-{
-  Serial.println("sending NTP packet...");
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  udp.beginPacket(address, 123); //NTP requests are to port 123
-  udp.write(packetBuffer, NTP_PACKET_SIZE);
-  udp.endPacket();
-}
-
-//*** FINISH NTP TIME Setting logic ***//
-
-void setwifi() {
-  // We start by connecting to a WiFi network
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
-     would try to act as both a client and an access-point and could cause
-     network-issues with your other WiFi-devices on your WiFi-network. */
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  delay(5000);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-//**************************** SETUP() ***************//
 void setup() {
+// put your setup code here, to run once:
 
-  // preparing GPIOs
-  pinMode(gpio0_pin, OUTPUT);
-  digitalWrite(gpio0_pin, LOW);
-  pinMode(gpio2_pin, OUTPUT);
-  digitalWrite(gpio2_pin, LOW);
-  
-  Serial.begin(115200);
-  delay(10);
+pinMode(r_pin,OUTPUT);
 
-  setwifi();
-  settime();
-  // web server setup
+pinMode(g_pin,OUTPUT);
 
-  webPage += "<h1>ESP8266 Web Server</h1><p>Socket #1 <a href=\"socket1On\"><button>ON</button></a>&nbsp;<a href=\"socket1Off\"><button>OFF</button></a></p>";
-  webPage += "<p>Socket #2 <a href=\"socket2On\"><button>ON</button></a>&nbsp;<a href=\"socket2Off\"><button>OFF</button></a></p>";
+pinMode(b_pin,OUTPUT);
 
-  if (mdns.begin("esp8266", WiFi.localIP())) {
-    Serial.println("MDNS responder started");
-  }
-
-  server.on("/", [](){
-    server.send(200, "text/html", webPage);
-  });
-  server.on("/socket1On", [](){
-    server.send(200, "text/html", webPage);
-    digitalWrite(gpio0_pin, HIGH);
-    delay(1000);
-  });
-  server.on("/socket1Off", [](){
-    server.send(200, "text/html", webPage);
-    digitalWrite(gpio0_pin, LOW);
-    delay(1000); 
-  });
-  server.on("/socket2On", [](){
-    server.send(200, "text/html", webPage);
-    digitalWrite(gpio2_pin, HIGH);
-    delay(1000);
-  });
-  server.on("/socket2Off", [](){
-    server.send(200, "text/html", webPage);
-    digitalWrite(gpio2_pin, LOW);
-    delay(1000); 
-  });
-  server.begin();
-  Serial.println("HTTP server started");
-  
 }
-
-int value = 0;
 
 void loop() {
-  
-  server.handleClient();
-  if ( !udp.parsePacket()){
-    gettime();  
-  }  
+// put your main code here, to run repeatedly:
+for(int fade = 0; fade <=255; fade +=5){
+analogWrite(r_pin,fade);
+delay(30);
+}
+for(int fade = 255; fade >= 0; fade -=5){
+analogWrite(r_pin,fade);
+delay(30);
+}
+for(int fade = 0; fade <=255; fade +=5){
+analogWrite(g_pin,fade);
+delay(30);
+}
+for(int fade = 255; fade >= 0; fade -=5){
+analogWrite(g_pin,fade);
+delay(30);
+}
+for(int fade = 0; fade <=255; fade +=5){
+analogWrite(b_pin,fade);
+delay(30);
+}
+for(int fade = 255; fade >= 0; fade -=5){
+analogWrite(b_pin,fade);
+delay(30);
+}
+
 }
 
